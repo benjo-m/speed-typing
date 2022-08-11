0042b16594a42d70832f09ad78343adbd9a64451
benjo-m <maglicbenjamin@gmail.com>
2022-08-12 00:32:22 +0200

    Basic functionality

---
+++ script.js
@@ -0,0 +1,50 @@
+// fetch("https://random-word-api.herokuapp.com/word?lang=de")
+
+let container = document.getElementById("container");
+let score = document.getElementById("score");
+let input = document.getElementById("input");
+let btn = document.getElementById("getWordBtn");
+let language = document.getElementById("language")
+
+let timer = document.getElementById("timer");
+let time = 10;
+
+language.onchange = getWord;
+
+score.textContent = 0
+let points = 0;
+
+function correct() {
+    if (container.textContent == input.value) {
+        points++;
+        score.textContent = points
+    }
+}
+
+function clear() {
+    points = 0;
+    score.textContent = points;
+    input.value = ""
+    input.focus()
+}
+
+function getWord() {
+    fetch(`https://random-word-api.herokuapp.com/word?number=200&lang=${language.value}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        container.textContent = data[0]
+        let counter = 1;
+        btn.addEventListener("click", e => {
+            e.preventDefault()
+            correct();
+            container.textContent = data[counter];
+            counter++
+            input.value = ""
+        })
+    })
+    clear()
+}
+
+getWord()
+
